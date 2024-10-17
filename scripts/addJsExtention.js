84fc92b02dddad17fa2f12247dfa5f56cfeffff1
wtricks <wtricks.ak@gmail.com>
2024-10-17 13:43:40 +0530

    removed extra code, added scripts.

---
+++ scripts/addJsExtention.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const directoryPath = path.join(__dirname, '../dist');
+
+function addJsExtensions(dirPath) {
+  fs.readdirSync(dirPath).forEach((file) => {
+    const filePath = path.join(dirPath, file);
+    const stat = fs.statSync(filePath);
+
+    if (stat.isDirectory()) {
+      addJsExtensions(filePath);
+    } else if (file.endsWith('.js')) {
+      let content = fs.readFileSync(filePath, 'utf8');
+
+      content = content.replace(
+        /(import\s.*from\s['"`])(\.\/|\.\.\/)([^'"`]+)(['"`])/g,
+        (match, p1, p2, p3, p4) => {
+          return p1 + p2 + p3 + '.js' + p4; // Append .js extension
+        }
+      );
+
+      fs.writeFileSync(filePath, content, 'utf8');
+    }
+  });
+}
+
+addJsExtensions(directoryPath.slice(1));
